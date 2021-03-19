21ab616e83226f2d71043868b3cc1eee44707f3a
Zacharias-Zhao <18611901469@163.com>
2021-03-19 16:48:04 +0800

    add trace_class trace_method

--- code/rouse/hook_java/demo1_1209.js
+++ code/rouse/hook_java/demo1_1209.js
@@ -20,8 +20,89 @@ function invokeNormal() {
     })
 }
 
+function invokeInit() {
+    Java.perform(function () {
+        var javaString = Java.use("java.lang.String")
+        var MoneyName = Java.use("com.xiaojianbang.app.Money").$new().name();
+        showToast(javaString.$new(MoneyName))
+    })
+}
+
+function invokeOverload() {
+    Java.perform(function () {
+        var javaString = Java.use("java.lang.String")
+        var result = Java.use("com.xiaojianbang.app.Utils").test(666);
+        showToast(javaString.$new(result))
+    })
+}
+
+function invokeObjectParm() {
+    Java.perform(function () {
+        var javaString = Java.use("java.lang.String")
+        var MoneyInstance = Java.use("com.xiaojianbang.app.Money").$new(javaString.$new("美元"), 200);
+        var result = Java.use("com.xiaojianbang.app.Utils").test(MoneyInstance);
+        showToast(javaString.$new(result))
+
+        // Java 数组
+        var StringArray = Java.array("java.lang.String", [javaString.$new("r0ysue  "), javaString.$new("you are the "), javaString.$new("best ")])
+        var retvl = Java.use("com.xiaojianbang.app.Utils").$new().myPrint(StringArray)
+        showToast(javaString.$new(retvl))
+    })
+}
+
+function invokeNative() {
+    Java.perform(function () {
+        var javaString = Java.use("java.lang.String")
+        var result = Java.use("com.xiaojianbang.app.NativeHelper").helloFromC();
+        showToast(javaString.$new(result));
+
+        result = Java.use("com.xiaojianbang.app.NativeHelper").add(100, 200, 300);
+        // result是int得先转成String
+        showToast(javaString.$new(String(result)));
+    })
+}
+
+function hookMD5() {
+    Java.perform(function () {
+        var targetClassMethod = "java.security.MessageDigest.getInstance"
+        // delim --> .
+        var delim = targetClassMethod.lastIndexOf(".");
+        if (delim === -1) return;
+        // targetClass --> java.security.MessageDigest
+        var targetClass = targetClassMethod.slice(0, delim)
+        // targetMethod --> getInstance
+        var targetMethod = targetClassMethod.slice(delim + 1, targetClassMethod.length)
+        var hook = Java.use(targetClass);
+        // 所有重载函数的数量
+        var overloadCount = hook[targetMethod].overloads.length;
+        // hook 遍历所有重载函数
+        for (var i = 0; i < overloadCount; i++) {
+            hook[targetMethod].overloads[i].implementation = function () {
+                console.warn("\n*** entered " + targetClassMethod);
+
+                // print args
+                if (arguments.length >= 0) {
+                    for (var j = 0; j < arguments.length; j++) {
+                        console.log("arg[" + j + "]: " + arguments[j]);
+                    }
+                }
+                var retval = this[targetMethod].apply(this, arguments);
+                console.log("\nretval: " + retval);
+                console.log(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));
+                return retval;
+            }
+        }
+    })
+}
+
+
 function main() {
-    invokeNormal()
+    // invokeNormal()
+    // invokeInit()
+    // invokeOverload()
+    // invokeObjectParm()
+    // invokeNative()
+    hookMD5()
 }
 
 setImmediate(main)
