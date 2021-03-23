cd5ec095e24e015a70a7cb66fced7a26af94a849
Zacharias-Zhao <18611901469@163.com>
2021-03-23 15:58:15 +0800

    保存证书

--- code/rouse/https/keystore_load.js
+++ code/rouse/https/keystore_load.js
@@ -1,8 +1,12 @@
-/*
-破解证书密码
-*/
 function hook_KeyStore_load() {
     Java.perform(function () {
+        var myArray = new Array(1024);
+        var i = 0
+        for (i = 0; i < myArray.length; i++) {
+            myArray[i] = 0x0;
+        }
+        var buffer = Java.array('byte', myArray);
+
         var StringClass = Java.use("java.lang.String");
         var KeyStore = Java.use("java.security.KeyStore");
         KeyStore.load.overload('java.security.KeyStore$LoadStoreParameter').implementation = function (arg0) {
@@ -13,6 +17,17 @@ function hook_KeyStore_load() {
         KeyStore.load.overload('java.io.InputStream', '[C').implementation = function (arg0, arg1) {
             console.log(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));
             console.log("KeyStore.load2:", arg0, arg1 ? StringClass.$new(arg1) : null);
+            // 保存证书
+            if (arg0) {
+                var file = Java.use("java.io.File").$new("/sdcard/Download/" + String(arg0) + ".p12");
+                var out = Java.use("java.io.FileOutputStream").$new(file);
+                var r;
+                while ((r = arg0.read(buffer)) > 0) {
+                    out.write(buffer, 0, r)
+                }
+                console.log("save success!")
+                out.close()
+            }
             this.load(arg0, arg1);
         };
 
