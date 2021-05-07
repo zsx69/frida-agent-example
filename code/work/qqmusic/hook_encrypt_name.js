520a4fec5039052abe34f4703226affc9b02baa1
Zacharias-Zhao <zhaoshouxin1126@gmail.com>
2021-05-07 15:08:39 +0800

    add code

--- code/work/qqmusic/hook_encrypt_name.js
+++ code/work/qqmusic/hook_encrypt_name.js
@@ -1,15 +1,26 @@
 function hook_encrypt_name() {
     Java.perform(function x() {
             console.log("java perform");
-            var ao = Java.use("com.tencent.qqmusiccommon.statistics.ao");
-            ao.getBase64String.implementation = function (v1) {
-                console.log("v1:" + v1);
-                var result = this.getBase64String(v1);
-                console.log("result:" + result);
+            var Base64 = Java.use("com.tencent.qqmusiccommon.util.Base64");
+            Base64.encode.implementation = function (v1) {
+                console.log("v1:" + bytesToString(v1));
+                var result = this.encode(v1);
+                console.log("result:" + bytesToString(result));
                 return result
             };
         }
     );
 }
+
+function bytesToString(arr) {
+    var str = '';
+    arr = new Uint8Array(arr);
+    for (var i in arr) {
+        str += String.fromCharCode(arr[i]);
+    }
+    return str;
+
+}
+
 
 setImmediate(hook_encrypt_name);
