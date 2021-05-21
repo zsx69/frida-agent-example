e9d6cb319c978700330c29147559cdc065cd7fc8
Zacharias-Zhao <zhaoshouxin1126@gmail.com>
2021-05-21 11:13:56 +0800

    add code

--- code/work/vip/hook_gs.js
+++ code/work/vip/hook_gs.js
@@ -17,4 +17,34 @@ function hook_gs() {
     );
 }
 
-setImmediate(hook_gs);
+
+function hook_security() {
+    Java.perform(function x() {
+            console.log("java perform");
+            var Security = Java.use("com.vip.vcsp.security.sign.VCSPSecurityConfig");
+            Security.gs.implementation = function (v1, v2, v3, v4) {
+                console.log("v1:" + v1.class);
+                console.log("v2:" + v2);
+                console.log("v3:" + v3);
+                console.log("v4:" + v4);
+                var result = this.gs(v1, v2, v3, v4);
+                console.log("result:" + result);
+                return result
+            };
+        }
+    );
+}
+
+
+function bytesToString(arr) {
+    var str = '';
+    arr = new Uint8Array(arr);
+    for (var i in arr) {
+        str += String.fromCharCode(arr[i]);
+    }
+    return str;
+
+}
+
+
+setImmediate(hook_security);
