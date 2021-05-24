14ebe0efdbd3360caa7ab9a92aaa91f944d7c323
Zacharias-Zhao <zhaoshouxin1126@gmail.com>
2021-05-24 10:41:49 +0800

    add code

--- code/work/cloud/hook_cloud.js
+++ code/work/cloud/hook_cloud.js
@@ -1,4 +1,4 @@
-function hook_request() {
+function hook_new() {
     Java.perform(function () {
         console.log("hook starting....")
         var nameClass = Java.use("com.netease.cloudmusic.utils.NeteaseMusicUtils")
@@ -13,19 +13,49 @@ function hook_request() {
     })
 }
 
-function bytesToString(arr) {
-    var str = '';
-    arr = new Uint8Array(arr);
-    for (var i in arr) {
-        str += String.fromCharCode(arr[i]);
-    }
-    return str;
 
+function hook_serialdata() {
+    Java.perform(function () {
+        console.log("hook starting....")
+        var nameClass = Java.use("com.netease.cloudmusic.utils.NeteaseMusicUtils")
+        nameClass.serialdata.implementation = function (v1, v2) {
+            console.log("v1:->", v1)
+            console.log("v2:->", v2)
+            var result = this.serialdata(v1, v2);
+            console.log("result:->", result)
+            return result
+        }
+    })
 }
 
+
+function hook_native() {
+    var JNI_address = Module.findExportByName('libpoison.so', 'AES_set_encrypt_key');
+    // 偏移量 = 符号地址 - so地址 = start（ida 最左边function_name 右拉 ）
+    console.log("JNI_address", JNI_address)
+
+     // var libnative_addr = Module.findBaseAddress('libpoison.so');
+     // var hashAddress = libnative_addr.add(0x00044E60);
+     // console.log("hashAddress", hashAddress);
+
+    Interceptor.attach(JNI_address, {
+        onEnter: function (args) {
+            var content =  Memory.readCString(args[0]);
+            console.log("content", content)
+        },
+        onLeave: function (retval) {
+            // 只能在native层使用
+            // console.log("retval", Java.vm.getEnv().getStringUtfChars(retval, null).readCString());
+        }
+    }
+    )
+}
 
 function main() {
-    hook_request()
+    hook_new()
+    hook_serialdata()
+    hook_native()
 }
+
 
 setImmediate(main)
