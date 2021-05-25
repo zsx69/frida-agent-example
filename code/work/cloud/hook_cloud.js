2c82994eae0284e578516fc2266037311b343b39
Zacharias-Zhao <zhaoshouxin1126@gmail.com>
2021-05-25 11:25:06 +0800

    cloud

--- code/work/cloud/hook_cloud.js
+++ code/work/cloud/hook_cloud.js
@@ -1,3 +1,4 @@
+// version 7.2.0
 function hook_new() {
     Java.perform(function () {
         console.log("hook starting....")
@@ -34,27 +35,76 @@ function hook_native() {
     // 偏移量 = 符号地址 - so地址 = start（ida 最左边function_name 右拉 ）
     console.log("JNI_address", JNI_address)
 
-     // var libnative_addr = Module.findBaseAddress('libpoison.so');
-     // var hashAddress = libnative_addr.add(0x00044E60);
-     // console.log("hashAddress", hashAddress);
+    // var libnative_addr = Module.findBaseAddress('libpoison.so');
+    // var hashAddress = libnative_addr.add(0x00044E60);
+    // console.log("hashAddress", hashAddress);
 
     Interceptor.attach(JNI_address, {
-        onEnter: function (args) {
-            var content =  Memory.readCString(args[0]);
-            console.log("content", content)
-        },
-        onLeave: function (retval) {
-            // 只能在native层使用
-            // console.log("retval", Java.vm.getEnv().getStringUtfChars(retval, null).readCString());
+            onEnter: function (args) {
+                var content = Memory.readCString(args[0]);
+                console.log("content", content)
+            },
+            onLeave: function (retval) {
+                // 只能在native层使用
+                // console.log("retval", Java.vm.getEnv().getStringUtfChars(retval, null).readCString());
+            }
         }
-    }
     )
 }
 
+/*
+sub_44764  args[1] 为url
+sub_43FFC  args[0] 已加盐
+sub_440A2  获取返回值
+ */
+
+function hook_salt() {
+    var libnative_addr = Module.findBaseAddress('libpoison.so');
+    var hashAddress = libnative_addr.add(0x00043FFC + 1);
+    console.log("hashAddress", hashAddress);
+
+
+    Interceptor.attach(hashAddress, {
+            onEnter: function (args) {
+                var content = Memory.readCString(args[0]);
+                console.log("content", content)
+            },
+            onLeave: function (retval) {
+                // 只能在native层使用
+            }
+        }
+    )
+}
+
+function hook_salt_result() {
+    var libnative_addr = Module.findBaseAddress('libpoison.so');
+    var hashAddress = libnative_addr.add(0x000440A2 + 1);
+    console.log("hashAddress", hashAddress);
+
+    var arg_address = null;
+
+    Interceptor.attach(hashAddress, {
+            onEnter: function (args) {
+                arg_address = args[0];
+                var content = Memory.readCString(arg_address);
+                console.log("content", content)
+            },
+            onLeave: function (retval) {
+                // c 语言可能不把结果直接返回而是重赋值给参数
+                console.log("retval1", Memory.readCString(arg_address));
+                console.log("retval2", Memory.readCString(retval));
+            }
+        }
+    )
+}
+
+
 function main() {
-    hook_new()
-    hook_serialdata()
-    hook_native()
+    // hook_new()
+    // hook_serialdata()
+    // hook_native()
+    // hook_salt()
+    // hook_salt_result()
 }
 
 
