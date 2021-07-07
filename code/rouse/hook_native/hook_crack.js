e92c580ea1068c2eb20662b584cafefc14ad8144
Zacharias-Zhao <zhaoshouxin1126@gmail.com>
2021-07-07 17:46:09 +0800

    patch code

--- code/rouse/hook_native/hook_crack.js
+++ code/rouse/hook_native/hook_crack.js
@@ -1,4 +1,5 @@
 // 32位寄存器位R 64位寄存器为X
+// com.yaotong.crackme
 
 function hook_r3() {
     // hook寄存器地址，得到对比的正确的base64
@@ -17,4 +18,26 @@ function hook_r3() {
 }
 
 
-setImmediate(hook_r3)
+function hook_crack() {
+    var libcrackme_addr = Module.findBaseAddress('libcrackme.so');
+    var left = libcrackme_addr.add(0x12A8);
+    console.log("left address ->", left)
+    console.log("left ->", hexdump(left))
+
+    var maxPatchSize = 64;
+    Memory.patchCode(left, maxPatchSize, function (code) {
+        console.log("code", code)
+        var cw = new ArmWriter(code, {pc: left});
+        cw.putBytes([
+            0x7a, 0x68, 0x61, 0x6f,
+            0x73, 0x68, 0x6f, 0x75,
+            0x78, 0x69, 0x6e, 0x6e
+        ]);
+        cw.flush()
+    });
+
+    console.log("addr_0x12A8_after ->", hexdump(left))
+}
+
+
+setImmediate(hook_crack)
